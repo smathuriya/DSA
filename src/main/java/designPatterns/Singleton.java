9ad507f8d7fa914d3a0594efc95f00d8f1d18f00
Saurabh Singh <saurabhsingh@miqdigital.com>
2023-06-03 17:25:25 +0530

    singleton pattern

---
+++ src/main/java/designPatterns/Singleton.java
@@ -0,0 +1,19 @@
+package designPatterns;
+
+public class Singleton {
+  private static Singleton obj;
+
+  private Singleton(){
+  }
+
+  public static Singleton getInstance(){
+    if(obj == null) {
+      synchronized (Singleton.class){
+        if (obj == null)
+          obj = new Singleton();
+      }
+      obj = new Singleton();
+    }
+    return obj;
+  }
+}
